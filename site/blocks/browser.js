b9ff6715a7ccecdac7f974781b960a81dedd76f3
Dan Finlay <dan@danfinlay.com>
2013-08-17 22:22:50 -0700

    Nearly working block server.

---
+++ site/blocks/browser.js
@@ -0,0 +1,22 @@
+blocklyToolbox.push({
+  name:'Browser',
+  blocks:['browser_alert']
+});
+
+Blockly.Language.browser_alert = {
+  helpUrl: 'http://www.example.com/',
+  init: function() {
+    this.setColour(210);
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.appendValueInput("alertText").setCheck("String").appendTitle("alert");
+    this.setTooltip('');
+  }
+};
+
+Blockly.JavaScript.browser_alert = function() {
+  var value_alerttext = Blockly.JavaScript.valueToCode(this, 'alertText', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = 'alert('+value_alerttext+')';
+  return code;
+}

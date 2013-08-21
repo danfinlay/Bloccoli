fd7235dece906d502dac470f06923080021a5cf6
Dan Finlay <dan@danfinlay.com>
2013-08-20 19:28:12 -0700

    Added modal & loading gif to share button.

--- site/js/bundle.js
+++ site/js/bundle.js
@@ -14,14 +14,13 @@ var pageGen = require('./pageGenerator');
 
 module.exports = function(toolBoxGen){
 
-  //Add blocks modal launcher:
+  //Add Blocks Button:
   $(window.parent.document).find('#addBlocksButton').on('click', function(e){
     e.preventDefault();
-    console.log("Attempting modal");
     $(window.parent.document).find("#addBlocksDialog").modal();
   });
 
-  //Run code:
+  //Run Button:
   var running = false;
   $(window.parent.document).find('#runButton').on('click', function(e){
 
@@ -42,9 +41,42 @@ module.exports = function(toolBoxGen){
 
   });
 
+  //Share Button:
   $(window.parent.document).find('#shareButton').on('click', function(e){
     e.preventDefault();
+    $(window.parent.document).find('#shareDialog .question').show();
+    $(window.parent.document).find("#shareDialog h3").text('Share this Project!');
+    $(window.parent.document).find('#shareLoading').hide();
+    $(window.parent.document).find("#shareDialog").modal();
+    //Blockly XML:
+
+
+    // $.ajax({
+    //   url:
+    // });
+
+  });
+
+  $(window.parent.document).find('#shareBlocks').on('click', function(e){
+    console.log("Editable clicked.");
+    e.preventDefault();
+
+    $(window.parent.document).find('#shareDialog .question').hide();
+    $(window.parent.document).find('#shareLoading').show();
+    $(window.parent.document).find("#shareDialog h3").text('Loading...');
+  });
+
+  $(window.parent.document).find('#shareResult').on('click', function(e){
+    e.preventDefault();
+
+    console.log("Result clicked.");
+
+    //Generated code:
     var code = window.Blockly.Generator.workspaceToCode('JavaScript');
+
+    $(window.parent.document).find('#shareDialog .question').hide();
+    $(window.parent.document).find('#shareLoading').show();
+    $(window.parent.document).find("#shareDialog h3").text('Loading...');
   });
 
 }
